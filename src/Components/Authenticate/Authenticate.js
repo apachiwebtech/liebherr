ef44d61abb0cfff51ef956b88a11036870654d1d
satyamsatkar <apachiwebtechnologyawt@gmail.com>
2024-12-17 18:41:31 +0530

    push

--- src/Components/Authenticate/Authenticate.js
+++ src/Components/Authenticate/Authenticate.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react';
 
 const Authenticate = () => {
+    useEffect(() => {
+        const hasReloaded = sessionStorage.getItem('hasReloaded');
 
-    const redirect = () => {
-       window.location.pathname = '/'
-    }
+        if (!hasReloaded) {
+            sessionStorage.setItem('hasReloaded', 'true');
+            window.location.reload(); // Reload the page once
+        } else {
+           
+            window.location.pathname = '/'; // Redirect after reload
+        }
+    }, []);
 
-    useEffect(() =>{
-        redirect()
-    },[])
-    
+    return null; // The component does not render anything
 };
 
 export default Authenticate;
